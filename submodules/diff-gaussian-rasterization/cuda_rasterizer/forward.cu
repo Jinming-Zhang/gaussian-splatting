8d2c66b430fb7fda191ab4144e2a0a56424fb018
jinming zhang <jinming.zhang47@gmail.com>
2026-08-11 21:07:48 -0700

    update render mode for new r_i term

--- submodules/diff-gaussian-rasterization/cuda_rasterizer/forward.cu
+++ submodules/diff-gaussian-rasterization/cuda_rasterizer/forward.cu
@@ -24,121 +24,55 @@ __device__ int RENDER_MODE = 0;
 // coefficients of each Gaussian to a simple RGB color.
 __device__ glm::vec3 computeColorFromSH(int idx, int deg, int max_coeffs, const glm::vec3 *means, glm::vec3 campos, const float *shs, bool *clamped)
 {
-  // The implementation is loosely based on code for
-  // "Differentiable Point-Based Radiance Fields for
-  // Efficient View Synthesis" by Zhang et al. (2022)
-  glm::vec3 pos = means[idx];
-  glm::vec3 dir = pos - campos;
-  dir = dir / glm::length(dir);
+	// The implementation is loosely based on code for 
+	// "Differentiable Point-Based Radiance Fields for 
+	// Efficient View Synthesis" by Zhang et al. (2022)
+	glm::vec3 pos = means[idx];
+	glm::vec3 dir = pos - campos;
+	dir = dir / glm::length(dir);
 
-  glm::vec3 *sh = ((glm::vec3 *)shs) + idx * max_coeffs;
-  glm::vec3 result{0.0f};
+	glm::vec3* sh = ((glm::vec3*)shs) + idx * max_coeffs;
+	glm::vec3 result = SH_C0 * sh[0];
 
-  switch (RENDER_MODE)
-  {
-  case 0:
-  {
-    result = SH_C0 * sh[0];
-    if (deg > 0)
-    {
-      float x = dir.x;
-      float y = dir.y;
-      float z = dir.z;
-      result = result - SH_C1 * y * sh[1] + SH_C1 * z * sh[2] - SH_C1 * x * sh[3];
+	if (deg > 0)
+	{
+		float x = dir.x;
+		float y = dir.y;
+		float z = dir.z;
+		result = result - SH_C1 * y * sh[1] + SH_C1 * z * sh[2] - SH_C1 * x * sh[3];
 
-      if (deg > 1)
-      {
-        float xx = x * x, yy = y * y, zz = z * z;
-        float xy = x * y, yz = y * z, xz = x * z;
-        result = result +
-                 SH_C2[0] * xy * sh[4] +
-                 SH_C2[1] * yz * sh[5] +
-                 SH_C2[2] * (2.0f * zz - xx - yy) * sh[6] +
-                 SH_C2[3] * xz * sh[7] +
-                 SH_C2[4] * (xx - yy) * sh[8];
+		if (deg > 1)
+		{
+			float xx = x * x, yy = y * y, zz = z * z;
+			float xy = x * y, yz = y * z, xz = x * z;
+			result = result +
+				SH_C2[0] * xy * sh[4] +
+				SH_C2[1] * yz * sh[5] +
+				SH_C2[2] * (2.0f * zz - xx - yy) * sh[6] +
+				SH_C2[3] * xz * sh[7] +
+				SH_C2[4] * (xx - yy) * sh[8];
 
-        if (deg > 2)
-        {
-          result = result +
-                   SH_C3[0] * y * (3.0f * xx - yy) * sh[9] +
-                   SH_C3[1] * xy * z * sh[10] +
-                   SH_C3[2] * y * (4.0f * zz - xx - yy) * sh[11] +
-                   SH_C3[3] * z * (2.0f * zz - 3.0f * xx - 3.0f * yy) * sh[12] +
-                   SH_C3[4] * x * (4.0f * zz - xx - yy) * sh[13] +
-                   SH_C3[5] * z * (xx - yy) * sh[14] +
-                   SH_C3[6] * x * (xx - 3.0f * yy) * sh[15];
-        }
-      }
-    }
-    result += 0.5f;
+			if (deg > 2)
+			{
+				result = result +
+					SH_C3[0] * y * (3.0f * xx - yy) * sh[9] +
+					SH_C3[1] * xy * z * sh[10] +
+					SH_C3[2] * y * (4.0f * zz - xx - yy) * sh[11] +
+					SH_C3[3] * z * (2.0f * zz - 3.0f * xx - 3.0f * yy) * sh[12] +
+					SH_C3[4] * x * (4.0f * zz - xx - yy) * sh[13] +
+					SH_C3[5] * z * (xx - yy) * sh[14] +
+					SH_C3[6] * x * (xx - 3.0f * yy) * sh[15];
+			}
+		}
+	}
+	result += 0.5f;
 
-    // RGB colors are clamped to positive values. If values are
-    // clamped, we need to keep track of this for the backward pass.
-    clamped[3 * idx + 0] = (result.x < 0);
-    clamped[3 * idx + 1] = (result.y < 0);
-    clamped[3 * idx + 2] = (result.z < 0);
-  }
-  break;
-  case 1: // reflectance
-  {
-    result = SH_C0 * sh[0];
-    result += 0.5f;
-
-    // RGB colors are clamped to positive values. If values are
-    // clamped, we need to keep track of this for the backward pass.
-    clamped[3 * idx + 0] = (result.x < 0);
-    clamped[3 * idx + 1] = (result.y < 0);
-    clamped[3 * idx + 2] = (result.z < 0);
-    return glm::max(result, 0.0f);
-  }
-  break;
-  case 2: // shading
-  {
-    if (deg > 0)
-    {
-      float x = dir.x;
-      float y = dir.y;
-      float z = dir.z;
-      result = result - SH_C1 * y * sh[1] + SH_C1 * z * sh[2] - SH_C1 * x * sh[3];
-
-      if (deg > 1)
-      {
-        float xx = x * x, yy = y * y, zz = z * z;
-        float xy = x * y, yz = y * z, xz = x * z;
-        result = result +
-                 SH_C2[0] * xy * sh[4] +
-                 SH_C2[1] * yz * sh[5] +
-                 SH_C2[2] * (2.0f * zz - xx - yy) * sh[6] +
-                 SH_C2[3] * xz * sh[7] +
-                 SH_C2[4] * (xx - yy) * sh[8];
-
-        if (deg > 2)
-        {
-          result = result +
-                   SH_C3[0] * y * (3.0f * xx - yy) * sh[9] +
-                   SH_C3[1] * xy * z * sh[10] +
-                   SH_C3[2] * y * (4.0f * zz - xx - yy) * sh[11] +
-                   SH_C3[3] * z * (2.0f * zz - 3.0f * xx - 3.0f * yy) * sh[12] +
-                   SH_C3[4] * x * (4.0f * zz - xx - yy) * sh[13] +
-                   SH_C3[5] * z * (xx - yy) * sh[14] +
-                   SH_C3[6] * x * (xx - 3.0f * yy) * sh[15];
-        }
-      }
-    }
-    result += 0.5f;
-
-    // RGB colors are clamped to positive values. If values are
-    // clamped, we need to keep track of this for the backward pass.
-    clamped[3 * idx + 0] = (result.x < 0);
-    clamped[3 * idx + 1] = (result.y < 0);
-    clamped[3 * idx + 2] = (result.z < 0);
-    return glm::max(result, 0.0f);
-  }
-  break;
-  default:
-    break;
-  }
-  return glm::max(result, 0.0f);
+	// RGB colors are clamped to positive values. If values are
+	// clamped, we need to keep track of this for the backward pass.
+	clamped[3 * idx + 0] = (result.x < 0);
+	clamped[3 * idx + 1] = (result.y < 0);
+	clamped[3 * idx + 2] = (result.z < 0);
+	return glm::max(result, 0.0f);
 }
 
 // Forward version of 2D covariance matrix computation
@@ -447,11 +381,6 @@ __global__ void __launch_bounds__(BLOCK_X *BLOCK_Y)
         continue;
       }
 
-      // if(collected_id[j]==72707)
-      // {
-      //   *minFeatureVal = reflect_factor[collected_id[j] * CHANNELS + 0];
-      //   *maxFeatureVal = reflect_factor[collected_id[j] * CHANNELS + 1];
-      // }
       // Eq. (3) from 3D Gaussian splatting paper.
       for (int ch = 0; ch < CHANNELS; ch++)
       {
@@ -464,7 +393,17 @@ __global__ void __launch_bounds__(BLOCK_X *BLOCK_Y)
         // float linearVal = (expf(tmp) - 1) / (2.71828f - 1);
 
         // C[ch] += linearVal * alpha * T;
-        C[ch] += (r_f * featureVal * alpha * T);
+        if(RENDER_MODE == 1) // reflectance
+        {
+          C[ch] += (r_f * alpha * T);
+        }
+        else if(RENDER_MODE == 2) // shading
+        {
+          C[ch] += (featureVal * alpha * T);
+        }
+        else {
+          C[ch] += (r_f * featureVal * alpha * T);
+        }
         // C[ch] += (featureVal * alpha * T);
       }
 
